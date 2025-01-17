2ab8678d1dee76b0a71c52c5f5417f8db894af74
Magomed Alimkhanov <scrythe@gmail.com>
2025-01-17 16:38:22 +0100

    feat: aproximate linear function

--- nn-regres.py
+++ nn-regres.py
@@ -33,26 +33,26 @@ def Mean_Squared_Error(ausgaben, lösungen):
 class Netzwerk:
     def __init__(selbst):
         anzahl_eingabe_neuronen = 1
-        anzahl_versteckte_neuronen = 6
+        # anzahl_versteckte_neuronen = 6
         anzahl_ausgabe_neuronen = 1
 
-        selbst.versteckte_schicht = Schicht(
-            anzahl_eingabe_neuronen, anzahl_versteckte_neuronen
-        )
-        selbst.versteckte_aktivierung = Sigmoid()
+        # selbst.versteckte_schicht = Schicht(
+        #     anzahl_eingabe_neuronen, anzahl_versteckte_neuronen
+        # )
+        # selbst.versteckte_aktivierung = Sigmoid()
         selbst.ausgabe_schicht = Schicht(
-            anzahl_versteckte_neuronen, anzahl_ausgabe_neuronen
+            anzahl_eingabe_neuronen, anzahl_ausgabe_neuronen
         )
-        selbst.ausgabe_aktivierung = Sigmoid()
+        selbst.ausgabe_aktivierung = ReLU()
         selbst.loss_funktion = Mean_Squared_Error
-        selbst.epochen = 5000
+        selbst.epochen = 50
         selbst.geschichte = []
 
     def vorwärtspropagierung(selbst, eingaben):
-        selbst.versteckte_schicht.vorwärts(eingaben)
-        selbst.versteckte_aktivierung.vorwärts(selbst.versteckte_schicht.ausgaben)
+        # selbst.versteckte_schicht.vorwärts(eingaben)
+        # selbst.versteckte_aktivierung.vorwärts(selbst.versteckte_schicht.ausgaben)
 
-        selbst.ausgabe_schicht.vorwärts(selbst.versteckte_aktivierung.ausgaben)
+        selbst.ausgabe_schicht.vorwärts(eingaben)
         selbst.ausgabe_aktivierung.vorwärts(selbst.ausgabe_schicht.ausgaben)
         return selbst.ausgabe_aktivierung.ausgaben
 
@@ -64,26 +64,28 @@ class Netzwerk:
             selbst.geschichte.append(ausgaben)
 
     def anwende_gradiant(selbst):
-        selbst.ausgabe_schicht.gewichte -= selbst.ausgabe_schicht.gradiantW * 0.5
-        selbst.ausgabe_schicht.bias -= selbst.ausgabe_schicht.gradiantB * 0.5
-        selbst.versteckte_schicht.gewichte -= selbst.versteckte_schicht.gradiantW * 0.5
-        selbst.versteckte_schicht.bias -= selbst.versteckte_schicht.gradiantB * 0.5
+        selbst.ausgabe_schicht.gewichte -= selbst.ausgabe_schicht.gradiantW * 0.1
+        selbst.ausgabe_schicht.bias -= selbst.ausgabe_schicht.gradiantB * 0.1
+        print(selbst.ausgabe_schicht.bias)
+        # selbst.versteckte_schicht.gewichte -= selbst.versteckte_schicht.gradiantW * 0.5
+        # selbst.versteckte_schicht.bias -= selbst.versteckte_schicht.gradiantB * 0.5
 
     def berechne_gradiant(selbst, eingaben, lösungen):
         ausgaben = selbst.vorwärtspropagierung(eingaben)
         loss = selbst.loss_funktion(ausgaben, lösungen)
+        selbst.loss = loss
         selbst.berechne_gradiant_w_schicht(
             selbst.ausgabe_schicht, loss, eingaben, lösungen
         )
         selbst.berechne_gradiant_b_schicht(
             selbst.ausgabe_schicht, loss, eingaben, lösungen
         )
-        selbst.berechne_gradiant_w_schicht(
-            selbst.versteckte_schicht, loss, eingaben, lösungen
-        )
-        selbst.berechne_gradiant_b_schicht(
-            selbst.versteckte_schicht, loss, eingaben, lösungen
-        )
+        # selbst.berechne_gradiant_w_schicht(
+        #     selbst.versteckte_schicht, loss, eingaben, lösungen
+        # )
+        # selbst.berechne_gradiant_b_schicht(
+        #     selbst.versteckte_schicht, loss, eingaben, lösungen
+        # )
 
     def berechne_gradiant_w_schicht(selbst, schicht, loss, eingaben, lösungen):
         h = 0.001
@@ -107,14 +109,18 @@ class Netzwerk:
 
 fig, ax = plt.subplots(figsize=(10, 5))
 (line,) = ax.plot([], [], label="NN Approximation", color="orange")
-ax.set_title("Neural Network Approximation of sin(x)")
 
-x = np.arange(1, 5, 0.1)
-x = (x).reshape(40, 1)
-y = np.sin(x)
+
+def f(x):
+    return 8 * x + 5
+
+
+x = np.arange(1, 5, 0.02)
+x = (x).reshape(200, 1)
+y = f(x)
 
 plt.plot(x, y, color="blue", label="True Function")
-plt.scatter(x, y, color="red", s=10, label="Data Points")
+# plt.scatter(x, y, color="red", s=10, label="Data Points")
 
 netzwerk = Netzwerk()
 netzwerk.trainieren(x, y)
@@ -127,12 +133,14 @@ def init():
 
 def update(epoche):
     line.set_data(x, netzwerk.geschichte[epoche])
-    ax.set_title(f"Epoch {epoche}/{netzwerk.epochen - 1}")
+    ax.set_title(f"Epoche {epoche}")
     return (line,)
 
 
-animation = animation.FuncAnimation(
-    fig, update, frames=(netzwerk.epochen), init_func=init, blit=True, interval=1
-)
+update(49)
+
+# animation = animation.FuncAnimation(
+#     fig, update, frames=(netzwerk.epochen), init_func=init, interval=50
+# )
 
 plt.show()

86cc59c5a750d5e1be34d1401fc55e3d0686a220
Magomed Alimkhanov <scrythe@gmail.com>
2025-01-18 15:54:17 +0100

    feat: refactor code and sorta fix something for future

--- aprox linear function.py
+++ aprox linear function.py
@@ -10,74 +10,75 @@ class Schicht:
 
     def vorwärts(selbst, eingaben):
         selbst.ausgaben = np.dot(eingaben, selbst.gewichte) + selbst.bias
+        return selbst.ausgaben
 
 
 class Sigmoid:
     def vorwärts(selbst, eingaben):
         selbst.ausgaben = 1 / (1 + np.exp(-eingaben))
+        return selbst.ausgaben
 
 
 class ReLU:
     def vorwärts(selbst, eingaben):
         selbst.ausgaben = np.maximum(0, eingaben)
+        return selbst.ausgaben
 
     def ableitung(selbst, eingaben):
         return 1 * (eingaben > 0)
 
 
-def Mean_Squared_Error(ausgaben, lösungen):
-    losses = np.power(ausgaben - lösungen, 2)
-    avg_loss = np.mean(losses)
-    return avg_loss
+class Mean_Squared_Error:
+    def berechnen(ausgaben, lösungen):
+        losses = np.power(ausgaben - lösungen, 2)
+        avg_loss = np.mean(losses)
+        return avg_loss
 
-
-def Mean_Squared_Error_Ableitung(ausgaben, lösungen):
-    return 2 * (ausgaben - lösungen)
+    def ableiten(ausgaben, lösungen):
+        return 2 * (ausgaben - lösungen)
 
 
 class Netzwerk:
     def __init__(selbst):
-        anzahl_eingabe_neuronen = 1
-        anzahl_ausgabe_neuronen = 1
+        netzwerk_größe = [1, 1, 1]
+        selbst.schicht1 = Schicht(netzwerk_größe[0], netzwerk_größe[1])
+        selbst.schicht2 = Schicht(netzwerk_größe[1], netzwerk_größe[2])
+        selbst.aktivierung1 = ReLU()
+        selbst.aktivierung2 = ReLU()
 
-        selbst.ausgabe_schicht = Schicht(
-            anzahl_eingabe_neuronen, anzahl_ausgabe_neuronen
-        )
-        selbst.ausgabe_aktivierung = ReLU()
         selbst.loss_funktion = Mean_Squared_Error
-        selbst.loss_ableitung = Mean_Squared_Error_Ableitung
 
-    def vorwärtspropagierung(selbst, eingaben):
-        selbst.ausgabe_schicht.vorwärts(eingaben)
-        selbst.ausgabe_aktivierung.vorwärts(selbst.ausgabe_schicht.ausgaben)
-        return selbst.ausgabe_aktivierung.ausgaben
+    def vorwärtsdurchlauf(selbst, eingaben):
+        # rohe_ausgaben_1 = selbst.schicht1.vorwärts(eingaben)
+        # aktivierte_ausgaben_1 = selbst.aktivierung1.vorwärts(rohe_ausgaben_1)
+        rohe_ausgaben_2 = selbst.schicht2.vorwärts(eingaben)
+        aktivierte_ausgaben_2 = selbst.aktivierung2.vorwärts(rohe_ausgaben_2)
+        return aktivierte_ausgaben_2
 
     def trainieren(selbst, eingaben, lösungen):
         geschichte = []
         loss = 100
-        # for _ in range(500):
-        while loss > 6:
-            ausgaben = selbst.vorwärtspropagierung(eingaben)
-            loss = selbst.loss_funktion(ausgaben, lösungen)
+        for _ in range(500):
+            # while loss > 6:
+            ausgaben = selbst.vorwärtsdurchlauf(eingaben)
+            loss = selbst.loss_funktion.berechnen(ausgaben, lösungen)
             geschichte.append(ausgaben)
             selbst.rückwärts(eingaben, lösungen)
         return geschichte
 
     def rückwärts(selbst, eingaben, lösungen):
-        dloss = selbst.loss_ableitung(selbst.ausgabe_aktivierung.ausgaben, lösungen)
-        drelu = dloss * selbst.ausgabe_aktivierung.ableitung(
-            selbst.ausgabe_aktivierung.ausgaben
-        )
+        dloss = selbst.loss_funktion.ableiten(selbst.aktivierung2.ausgaben, lösungen)
+        drelu = dloss * selbst.aktivierung2.ableitung(selbst.schicht2.ausgaben)
         dweight = drelu * eingaben
         dweight = np.mean(dweight)
         dbias = np.mean(drelu)
 
-        selbst.ausgabe_schicht.gewichte -= dweight * 0.1
-        selbst.ausgabe_schicht.bias -= dbias * 0.1
+        selbst.schicht2.gewichte -= dweight * 0.1
+        selbst.schicht2.bias -= dbias * 0.1
 
 
 def f(x):
-    return 3 * x * x
+    return 3 * x**4
 
 
 eingaben = np.arange(0, 5, 0.5).reshape(10, 1)
@@ -110,12 +111,12 @@ def update(epoche):
 framge_range = range(0, len(geschichte), 5)
 
 ani = animation.FuncAnimation(
-    fig, update, frames=framge_range, init_func=init, interval=50, repeat=False
+    fig, update, frames=framge_range, init_func=init, interval=20, repeat=False
 )
 
-# writer = animation.PillowWriter(
-#     fps=15, metadata=dict(artist="Magomed Alimkhanov"), bitrate=1800
-# )
-# ani.save("hm.gif", writer)
+writer = animation.FFMpegWriter(
+    fps=15, metadata=dict(artist="Magomed Alimkhanov"), bitrate=1800
+)
+ani.save("hm.gif", writer)
 
 plt.show()

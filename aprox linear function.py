4af9db99e0c3833538533eeafd75e02f6b0d53bb
Magomed Alimkhanov <scrythe@gmail.com>
2025-01-19 20:53:31 +0100

    feat: use sentdex backprop with own and use sorta optimal weights for test

--- aprox linear function.py
+++ aprox linear function.py
@@ -9,103 +9,87 @@ class Schicht:
         selbst.bias = np.zeros((1, anzahl_neuronen))
 
     def vorwärts(selbst, eingaben):
+        selbst.eingaben = eingaben
         selbst.ausgaben = np.dot(eingaben, selbst.gewichte) + selbst.bias
         return selbst.ausgaben
 
-
-class Sigmoid:
-    def vorwärts(selbst, eingaben):
-        selbst.ausgaben = 1 / (1 + np.exp(-eingaben))
-        return selbst.ausgaben
+    def rückwärts(selbst, dwerte):
+        selbst.dgewichte = np.dot(selbst.eingaben.T, dwerte)
+        selbst.dbias = np.sum(dwerte, axis=0, keepdims=True)
+        selbst.deingaben = np.dot(dwerte, selbst.gewichte.T)
 
 
 class ReLU:
     def vorwärts(selbst, eingaben):
+        selbst.eingaben = eingaben
         selbst.ausgaben = np.maximum(0, eingaben)
         return selbst.ausgaben
 
-    def ableiten(selbst, eingaben):
-        return 1 * (eingaben > 0)
+    def rückwärts(selbst, dwerte):
+        selbst.deingaben = dwerte * (selbst.eingaben > 0)
 
 
 class Mean_Squared_Error:
-    def berechnen(ausgaben, lösungen):
+    def vorwärts(ausgaben, lösungen):
         losses = np.power(ausgaben - lösungen, 2)
-        avg_loss = np.mean(losses)
-        return avg_loss
+        return np.mean(losses)
 
-    def ableiten(ausgaben, lösungen):
-        return 2 * (ausgaben - lösungen)
+    def rückwärts(ausgaben, lösungen):
+        dloss = 2 * (ausgaben - lösungen)
+        return dloss / len(dloss)
+
+
+class SGD:
+    def __init__(selbst, learn_rate):
+        selbst.lern_rate = learn_rate
+
+    def aktualisiere_parameter(selbst, schicht):
+        schicht.gewichte -= schicht.dgewichte * selbst.lern_rate
+        schicht.bias -= schicht.dbias * selbst.lern_rate
 
 
 class Netzwerk:
     def __init__(selbst):
-        netzwerk_größe = [1, 8, 9, 1]
-        selbst.schicht1 = Schicht(netzwerk_größe[0], netzwerk_größe[1])
-        selbst.schicht2 = Schicht(netzwerk_größe[1], netzwerk_größe[2])
-        selbst.schicht3 = Schicht(netzwerk_größe[2], netzwerk_größe[3])
+        lern_rate = 0.1
+        selbst.schicht1 = Schicht(1, 8)
         selbst.aktivierung1 = ReLU()
+        selbst.schicht2 = Schicht(8, 8)
         selbst.aktivierung2 = ReLU()
-
+        selbst.schicht3 = Schicht(8, 1)
         selbst.loss_funktion = Mean_Squared_Error
+        selbst.optimierer = SGD(lern_rate)
 
-    def vorwärtsdurchlauf(selbst, eingaben):
+    def vorwärts_durchlauf(selbst, eingaben):
         rohe_ausgaben_1 = selbst.schicht1.vorwärts(eingaben)
         aktivierte_ausgaben_1 = selbst.aktivierung1.vorwärts(rohe_ausgaben_1)
         rohe_ausgaben_2 = selbst.schicht2.vorwärts(aktivierte_ausgaben_1)
         aktivierte_ausgaben_2 = selbst.aktivierung2.vorwärts(rohe_ausgaben_2)
         rohe_ausgaben_3 = selbst.schicht3.vorwärts(aktivierte_ausgaben_2)
-        return rohe_ausgaben_3
+        loss = selbst.loss_funktion.vorwärts(rohe_ausgaben_3, lösungen)
+        return rohe_ausgaben_3, loss
 
     def trainieren(selbst, eingaben, lösungen):
         geschichte = []
-        loss = 100
+        # loss = 100
         for _ in range(1000):
             # while loss > 6:
-            ausgaben = selbst.vorwärtsdurchlauf(eingaben)
-            loss = selbst.loss_funktion.berechnen(ausgaben, lösungen)
+            ausgaben, loss = selbst.vorwärts_durchlauf(eingaben)
             # print(loss)
+            selbst.rückwärts_durchlauf(eingaben, lösungen)
+            selbst.optimierer.aktualisiere_parameter(selbst.schicht3)
+            selbst.optimierer.aktualisiere_parameter(selbst.schicht2)
+            selbst.optimierer.aktualisiere_parameter(selbst.schicht1)
             geschichte.append(ausgaben)
-            selbst.rückwärts(eingaben, lösungen)
         return geschichte
 
-    def rückwärts(selbst, eingaben, lösungen):
-        lern_rate = 0.1
-        ausgaben = selbst.vorwärtsdurchlauf(eingaben)
-        loss = selbst.loss_funktion.berechnen(ausgaben, lösungen)
-
-        dloss = selbst.loss_funktion.ableiten(selbst.schicht3.ausgaben, lösungen)
-        dweight1 = dloss * selbst.aktivierung2.ausgaben
-        dweight1 = np.mean(dweight1, axis=0, keepdims=True).T
-        dbias1 = np.mean(dloss)
-
-        d_ausgabe_1 = np.dot(dloss, selbst.schicht3.gewichte.T)
-        d_relu_2 = d_ausgabe_1 * selbst.aktivierung2.ableiten(selbst.schicht2.ausgaben)
-        d_relu_2 = np.mean(d_relu_2, axis=0, keepdims=True)
-        dweight2 = np.dot(
-            d_relu_2.T, np.mean(selbst.aktivierung1.ausgaben, axis=0, keepdims=True)
-        )
-        dweight2 = np.sum(dweight2, axis=0)
-        dbias2 = np.sum(d_relu_2, axis=1)
-
-        selbst.schicht2.bias += 0.0001
-        ausgaben = selbst.vorwärtsdurchlauf(eingaben)
-        delta_loss = selbst.loss_funktion.berechnen(ausgaben, lösungen) - loss
-        print(delta_loss / 0.0001)
-        print(np.sum(dbias2))
-
-        d_ausgabe_2 = np.dot(d_ausgabe_1, selbst.schicht2.gewichte.T)
-        d_relu_3 = d_ausgabe_2 * selbst.aktivierung1.ableiten(selbst.schicht2.ausgaben)
-        dweight3 = d_relu_3 * eingaben
-        dweight3 = np.mean(dweight3, axis=0)
-        dbias3 = np.mean(d_relu_3, axis=0)
-
-        selbst.schicht3.gewichte -= dweight1 * lern_rate
-        selbst.schicht3.bias -= dbias1 * lern_rate
-        selbst.schicht2.gewichte -= dweight2 * lern_rate
-        selbst.schicht2.bias -= dbias2 * lern_rate
-        selbst.schicht1.gewichte -= dweight3 * lern_rate
-        selbst.schicht1.bias -= dbias3 * lern_rate
+    def rückwärts_durchlauf(selbst, eingaben, lösungen):
+        dloss = selbst.loss_funktion.rückwärts(selbst.schicht3.ausgaben, lösungen)
+        loss = selbst.loss_funktion.vorwärts(selbst.schicht3.ausgaben, lösungen)
+        selbst.schicht3.rückwärts(dloss)
+        selbst.aktivierung2.rückwärts(selbst.schicht3.deingaben)
+        selbst.schicht2.rückwärts(selbst.aktivierung2.deingaben)
+        selbst.aktivierung1.rückwärts(selbst.schicht2.deingaben)
+        selbst.schicht1.rückwärts(selbst.aktivierung1.deingaben)
 
 
 def f(x):
@@ -113,7 +97,7 @@ def f(x):
 
 
 eingaben = np.arange(0, 5, 0.5).reshape(10, 1)
-eingaben = np.arange(2, 10, 5).reshape(2, 1)
+# eingaben = np.arange(2, 10, 5).reshape(2, 1)
 lösungen = f(eingaben)
 
 
@@ -126,7 +110,68 @@ plt.plot(eingaben, lösungen, "x", color="red", label="Trainings Daten")
 
 
 netzwerk = Netzwerk()
-geschichte = netzwerk.trainieren(eingaben, lösungen)
+
+
+def optimal():
+    netzwerk.schicht1.gewichte = [
+        [1.2072, -0.7405, 1.3646, 0.5692, 1.6801, -0.1454, -0.4372, 1.1967]
+    ]
+
+    netzwerk.schicht1.bias = [
+        [
+            -2.0098,
+            -0.0290,
+            0.7159,
+            0.5741,
+            0.7426,
+            -0.4148,
+            1.9866,
+            0.5851,
+        ]
+    ]
+
+    netzwerk.schicht2.gewichte = [
+        [1.2142, 0.8653, -0.2282, -0.2028, 0.9729, -1.3938, 0.0808, 0.2845],
+        [-0.2687, 0.1175, 0.0131, -0.0079, -0.1701, 0.2546, 0.1974, 0.24],
+        [0.2806, 0.7469, -0.114, 0.0514, 0.683, 0.4402, -0.0339, -0.2846],
+        [0.4742, 0.5785, 0.0451, -0.1938, 0.2171, 0.6416, -0.2316, -0.058],
+        [0.5731, 0.7834, -0.1861, 0.0798, 0.9531, -0.0173, -0.2048, 0.0684],
+        [0.1354, 0.0085, -0.0252, 0.1742, 0.1655, -0.2682, 0.1864, -0.0308],
+        [-2.6353, -2.2848, -0.1197, -0.2494, -1.8291, 1.2427, -0.1891, -0.2045],
+        [0.7434, 0.4293, 0.171, -0.409, 0.9035, -0.0922, -0.2061, -0.2992],
+    ]
+
+    netzwerk.schicht2.bias = [
+        [
+            0.2191,
+            0.1604,
+            -0.0102,
+            -0.4476,
+            -0.0385,
+            0.4184,
+            -0.3466,
+            0.3397,
+        ]
+    ]
+
+    netzwerk.schicht3.gewichte = [
+        [0.8571],
+        [1.0345],
+        [-0.0774],
+        [-0.1742],
+        [0.9437],
+        [-0.9353],
+        [-0.0455],
+        [-0.2133],
+    ]
+
+    netzwerk.schicht3.bias = [[0.4596]]
+
+
+optimal()
+# geschichte = netzwerk.trainieren(eingaben, lösungen)
+(ausgaben, _) = netzwerk.vorwärts_durchlauf(eingaben)
+geschichte = [ausgaben]
 
 
 def init():

396f3189d72a780cf193e0aad078ff16b9ebcc86
Magomed Alimkhanov <scrythe@gmail.com>
2025-01-19 15:16:12 +0100

    feat: attempt to make backprop work for bigger network (not working)

--- aprox linear function.py
+++ aprox linear function.py
@@ -40,9 +40,10 @@ class Mean_Squared_Error:
 
 class Netzwerk:
     def __init__(selbst):
-        netzwerk_größe = [1, 3, 1]
+        netzwerk_größe = [1, 8, 9, 1]
         selbst.schicht1 = Schicht(netzwerk_größe[0], netzwerk_größe[1])
         selbst.schicht2 = Schicht(netzwerk_größe[1], netzwerk_größe[2])
+        selbst.schicht3 = Schicht(netzwerk_größe[2], netzwerk_größe[3])
         selbst.aktivierung1 = ReLU()
         selbst.aktivierung2 = ReLU()
 
@@ -53,7 +54,8 @@ class Netzwerk:
         aktivierte_ausgaben_1 = selbst.aktivierung1.vorwärts(rohe_ausgaben_1)
         rohe_ausgaben_2 = selbst.schicht2.vorwärts(aktivierte_ausgaben_1)
         aktivierte_ausgaben_2 = selbst.aktivierung2.vorwärts(rohe_ausgaben_2)
-        return aktivierte_ausgaben_2
+        rohe_ausgaben_3 = selbst.schicht3.vorwärts(aktivierte_ausgaben_2)
+        return rohe_ausgaben_3
 
     def trainieren(selbst, eingaben, lösungen):
         geschichte = []
@@ -68,41 +70,50 @@ class Netzwerk:
         return geschichte
 
     def rückwärts(selbst, eingaben, lösungen):
-        lern_rate = 0.0001
+        lern_rate = 0.1
         ausgaben = selbst.vorwärtsdurchlauf(eingaben)
         loss = selbst.loss_funktion.berechnen(ausgaben, lösungen)
-        dloss = selbst.loss_funktion.ableiten(selbst.aktivierung2.ausgaben, lösungen)
-        drelu = dloss * selbst.aktivierung2.ableiten(selbst.schicht2.ausgaben)
-        dweight = drelu * selbst.aktivierung1.ausgaben
-        dweight = np.mean(dweight, axis=0, keepdims=True).T
-        dbias = np.mean(drelu)
 
-        # selbst.schicht2.gewichte -= dweight * lern_rate
-        # selbst.schicht2.bias -= dbias * lern_rate
+        dloss = selbst.loss_funktion.ableiten(selbst.schicht3.ausgaben, lösungen)
+        dweight1 = dloss * selbst.aktivierung2.ausgaben
+        dweight1 = np.mean(dweight1, axis=0, keepdims=True).T
+        dbias1 = np.mean(dloss)
 
-        d_ausgabe_1 = np.dot(drelu, selbst.schicht2.gewichte.T)
-        # dweight = d_ausgabe_1 * eingaben
-        # dweight = np.mean(dweight, axis=0)
-        # dbias = np.mean(d_ausgabe_1, axis=0)
-
-        selbst.aktivierung1.ausgaben += 0.001
-        rohe_ausgaben_2 = selbst.schicht2.vorwärts(selbst.aktivierung1.ausgaben)
-        aktivierte_ausgaben_2 = selbst.aktivierung2.vorwärts(rohe_ausgaben_2)
-        delta_loss = (
-            selbst.loss_funktion.berechnen(aktivierte_ausgaben_2, lösungen) - loss
+        d_ausgabe_1 = np.dot(dloss, selbst.schicht3.gewichte.T)
+        d_relu_2 = d_ausgabe_1 * selbst.aktivierung2.ableiten(selbst.schicht2.ausgaben)
+        d_relu_2 = np.mean(d_relu_2, axis=0, keepdims=True)
+        dweight2 = np.dot(
+            d_relu_2.T, np.mean(selbst.aktivierung1.ausgaben, axis=0, keepdims=True)
         )
-        print(delta_loss / 0.001)
-        print(np.mean(np.sum(d_ausgabe_1, axis=1)))
+        dweight2 = np.sum(dweight2, axis=0)
+        dbias2 = np.sum(d_relu_2, axis=1)
 
-        # selbst.schicht1.gewichte -= dweight * lern_rate
-        # selbst.schicht1.bias -= dbias * lern_rate
+        selbst.schicht2.bias += 0.0001
+        ausgaben = selbst.vorwärtsdurchlauf(eingaben)
+        delta_loss = selbst.loss_funktion.berechnen(ausgaben, lösungen) - loss
+        print(delta_loss / 0.0001)
+        print(np.sum(dbias2))
+
+        d_ausgabe_2 = np.dot(d_ausgabe_1, selbst.schicht2.gewichte.T)
+        d_relu_3 = d_ausgabe_2 * selbst.aktivierung1.ableiten(selbst.schicht2.ausgaben)
+        dweight3 = d_relu_3 * eingaben
+        dweight3 = np.mean(dweight3, axis=0)
+        dbias3 = np.mean(d_relu_3, axis=0)
+
+        selbst.schicht3.gewichte -= dweight1 * lern_rate
+        selbst.schicht3.bias -= dbias1 * lern_rate
+        selbst.schicht2.gewichte -= dweight2 * lern_rate
+        selbst.schicht2.bias -= dbias2 * lern_rate
+        selbst.schicht1.gewichte -= dweight3 * lern_rate
+        selbst.schicht1.bias -= dbias3 * lern_rate
 
 
 def f(x):
-    return 3 * x**4
+    return 3 * x**2
 
 
 eingaben = np.arange(0, 5, 0.5).reshape(10, 1)
+eingaben = np.arange(2, 10, 5).reshape(2, 1)
 lösungen = f(eingaben)
 
 
